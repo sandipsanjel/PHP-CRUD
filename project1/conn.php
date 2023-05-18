285650f2dba85b6e5c46f8a7e546a3972c3ff74a
sandip <sandipsanjel@users.noreply.github.com>
2023-05-18 16:27:33 +0545

    project1deleted

--- project1/conn.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$conn=mysqli_connect("localhost","root","","registerdb");
-if($conn){
-    echo "conected";
-}
-else{
-    echo "error";
-}
--- project1/del.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id=$_GET["id"];
-echo $id;
-include("conn.php");
-$sql="delete from task where id='$id'";
-$qry=mysqli_query($conn,$sql);
-header("Location:list.php")
-?>
